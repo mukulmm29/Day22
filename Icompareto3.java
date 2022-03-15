e2ace912ad53c49ee97cd423670fce0c74c3e141
mukulmm29 <98878287+mukulmm29@users.noreply.github.com>
2022-03-15 18:12:36 +0530

    .

---
+++ Icompareto3.java
@@ -0,0 +1,13 @@
+public class Icompareto3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String x ="m";
+		String y = "g";
+		Integer m =15; 
+		Integer g = 20;
+		System.out.println("String Result Of Compare " +x.compareTo(y));
+		System.out.println("Integer Result Of Compare " +m.compareTo(g));
+	}
+
+}
